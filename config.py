194e6f5602de16145d873eb6b4d64b5305d3a848
Imtiaz9800 <120592668+Imtiaz9800@users.noreply.github.com>
2025-05-11 03:59:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -26,7 +26,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "5100345229"))
 PORT = os.environ.get("PORT", "8080")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://fakemailcloud3:aZMCYB9Uh0xE7Gox@cluster0.pf4nmcc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://fakemailcloud3:aZMCYB9Uh0xE7Gox@cluster0.pf4nmcc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 DATABASE_NAME  - Cluster0")
 DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 
